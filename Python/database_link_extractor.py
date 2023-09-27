d5e2f8c5eae70a5b0474b20c6d97af5080e3613a
noobiiii <noobiiii@users.noreply.github.com>
2023-09-27 19:06:55 +0200

    Add options with different output types

--- Python/database_link_extractor.py
+++ Python/database_link_extractor.py
@@ -1,13 +1,27 @@
 import sqlite3
 import re
+import optparse
 
-dbFile = 'dbname.db'  # Input database
-outputDBFile = 'output_database.db'  # Output database
+
+parser = optparse.OptionParser()
+parser.add_option('-i', '--input', dest='input', help='Input database file')
+parser.add_option('-o', '--output', dest='output', help='Output database file')
+parser.add_option('-t', '--type', dest='type', help='sqlite, txt')
+(options, args) = parser.parse_args()
+
+if not options.input:
+    parser.error('Input database file not provided')
+if not options.output:
+    parser.error('Output database file not provided')    
+
+dbFile = options.input  # Input database
+outputDBFile = options.output  # Output database
 
 # Function to extract HTTPS links from text
 def extract_https_links(text):
     https_pattern = r'https://[^\s]+'
     return re.findall(https_pattern, text)
+
 
 # Establish a connection to the input database
 conn_source = sqlite3.connect(dbFile)
@@ -33,22 +47,26 @@ for table in tables:
 # Close the connection to the input database
 conn_source.close()
 
-# Establish a connection to the output database
-conn_target = sqlite3.connect(outputDBFile)
-cursor_target = conn_target.cursor()
+if options.type == 'txt':
+    with open(options.output + '.txt', 'w') as f:
+        for link in https_links:
+            f.write(f'{link}\n')
+else:
 
-# Create a table in the output database if it doesn't exist
-cursor_target.execute('''CREATE TABLE IF NOT EXISTS https_links (link TEXT)''')
+    # Establish a connection to the output database
+    conn_target = sqlite3.connect(outputDBFile + '.db')
+    cursor_target = conn_target.cursor()
 
-# Check if the links already exist in the output database and only add them if they do not exist
-for link in https_links:
-    cursor_target.execute("SELECT link FROM https_links WHERE link=?", (link,))
-    existing_link = cursor_target.fetchone()
-    if not existing_link:
-        cursor_target.execute("INSERT INTO https_links (link) VALUES (?)", (link,))
+    # Create a table in the output database if it doesn't exist
+    cursor_target.execute('''CREATE TABLE IF NOT EXISTS https_links (link TEXT)''')
 
-# Save changes in the output database and close the connection
-conn_target.commit()
-conn_target.close()
+    # Check if the links already exist in the output database and only add them if they do not exist
+    for link in https_links:
+        cursor_target.execute("SELECT link FROM https_links WHERE link=?", (link,))
+        existing_link = cursor_target.fetchone()
+        if not existing_link:
+            cursor_target.execute("INSERT INTO https_links (link) VALUES (?)", (link,))
 
-
+    # Save changes in the output database and close the connection
+    conn_target.commit()
+    conn_target.close()
